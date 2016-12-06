8561c0ec5623a2d8729a54cdbea830bdebb8ffc4
Jacob Bublitz <Jacob@Bublitz.com>
2016-12-05 16:46:44 -0800

    More comments in client example and made code more memory safe

--- examples/rcon_client.c
+++ examples/rcon_client.c
@@ -32,16 +32,58 @@ static void _print_error(RCONerror error) {
 	getc(stdin);
 }
 
+/**
+ * \brief Reads a line from stdin.
+ * 
+ * \note Returned value must be free'd.
+ * 
+ * \returns The read line
+ */
+static const char *_read_line(void) {
+	char *buffer = malloc(sizeof(char)), *tempBuffer;
+	size_t bufferLength = sizeof(char);
+	size_t filledBuffer = 0;
+	
+	if (buffer == NULL) {
+		return NULL;
+	}
+	
+	for (;;) {
+		int c = fgetc(stdin);
+		if (c == EOF || c == '\n') {
+			break;
+		}
+		buffer[filledBuffer++] = (char)c;
+		
+		if (filledBuffer + 1 > bufferLength) {
+			tempBuffer = realloc(buffer, bufferLength *= 2);
+			if (tempBuffer == NULL) {
+				free(buffer);
+				return NULL;
+			}
+			
+			buffer = tempBuffer;
+		}
+	}
+	buffer[filledBuffer] = '\0';
+	
+	return buffer;
+}
+
+/**
+ * \brief Prints out the usage of the command.
+ */
 static void _usage(void) {
-	printf("Usage: rcon_example_client <HOSTNAME> <PORT> <PASSWORD>\n");
+	printf("Usage: RCON_example_client <HOSTNAME> <PORT> <PASSWORD>\n");
 	exit(0);
 }
 
 int main(int argc, const char **argv) {
+	int i;
 	RCONclient *client;
-	char input[512];
+	const char *input;
 	char *response;
-
+	
 	if (argc != 4) {
 		_usage();
 	}
@@ -72,9 +114,14 @@ int main(int argc, const char **argv) {
 
 	printf("[RCON]: Type a command or '!quit' to exit.\n");
 	do {
-		memset(input, 0, 512);
-		gets(input);
-
+		input = _read_line();
+		if (input == NULL) {
+			printf("[RCON/Error]: Failed to read input.\n");
+			rconClientDisconnect(client);
+			rconTerminate();
+			return -1;
+		}
+		
 		if (strcmp("!quit", input) == 0) {
 			break;
 		}
